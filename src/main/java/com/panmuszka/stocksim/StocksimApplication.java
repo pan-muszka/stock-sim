ad893a479153909ab5af1989e90ca2f9ae76e3a4
halkunex <jakubmucha@outlook.com>
2025-05-12 12:18:48 +0200

    Spring boot template project creation

---
+++ src/main/java/com/panmuszka/stocksim/StocksimApplication.java
@@ -0,0 +1,13 @@
+package com.panmuszka.stocksim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/panmuszka/stocksim/StocksimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panmuszka.stocksim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
